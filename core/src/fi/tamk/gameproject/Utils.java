45f7fda9d0fcff0c8f3f95ec583402ed87e1ee38
Miko Kauhanen <miko.kauhanen@gmail.com>
2019-03-02 18:58:55 +0200

    Added random number method

---
+++ core/src/fi/tamk/gameproject/Utils.java
@@ -0,0 +1,16 @@
+package fi.tamk.gameproject;
+
+import com.badlogic.gdx.math.MathUtils;
+
+/*
+This class holds general utilities
+ */
+
+public class Utils {
+
+    private float randomNumber(float min, float max) {
+        float random = MathUtils.random(min, max);
+        return random;
+    }
+
+}
