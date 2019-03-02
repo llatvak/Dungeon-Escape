669f6a1697f5a49840a061fb72ba4cc76199025c
Miko Kauhanen <miko.kauhanen@gmail.com>
2019-03-02 19:15:21 +0200

    Updated main game class

--- core/src/fi/tamk/gameproject/DungeonEscape.java
+++ core/src/fi/tamk/gameproject/DungeonEscape.java
@@ -1,33 +1,61 @@
 package fi.tamk.gameproject;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class DungeonEscape extends ApplicationAdapter {
+import java.util.Map;
+
+public class DungeonEscape extends Game {
+
 	SpriteBatch batch;
-	Texture img;
-	
+
+	// Fonts
+	BitmapFont fontRoboto;
+	GlyphLayout layout;
+	FreeTypeFontGenerator fontGenerator;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		MapScreen mapScreen = new MapScreen(this);
+		setScreen(mapScreen);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		super.render();
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	public SpriteBatch getBatch() {
+		return batch;
+	}
+
+	public BitmapFont getFont() {
+		//System.out.println("getting font");
+		return fontRoboto;
+	}
+
+	public GlyphLayout getLayout() {
+		return layout;
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
