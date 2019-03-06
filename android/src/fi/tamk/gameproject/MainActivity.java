035d51f1c27c55223a1c7745979974b08a627db5
Miko Kauhanen <miko.kauhanen@gmail.com>
2019-03-06 08:20:18 +0200

    Pedometer testing

--- android/src/fi/tamk/gameproject/MainActivity.java
+++ android/src/fi/tamk/gameproject/MainActivity.java
@@ -1,29 +1,69 @@
 package fi.tamk.gameproject;
 
-import sun.management.Sensor;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-public class StepMainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
+    private TextView textView;
     private StepDetector simpleStepDetector;
     private SensorManager sensorManager;
     private Sensor accel;
     private static final String TEXT_NUM_STEPS = "Number of Steps: ";
     private int numSteps;
-
+    TextView TvSteps;
+    Button BtnStart;
+    Button BtnStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
 
         // Get an instance of the SensorManager
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         simpleStepDetector = new StepDetector();
         simpleStepDetector.registerListener(this);
+
+        TvSteps = (TextView) findViewById(R.id.tv_steps);
+        BtnStart = (Button) findViewById(R.id.btn_start);
+        BtnStop = (Button) findViewById(R.id.btn_stop);
+
+
+
+        BtnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                numSteps = 0;
+                sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+
+            }
+        });
+
+
+        BtnStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                sensorManager.unregisterListener(MainActivity.this);
+
+            }
+        });
+
+
+
     }
+
 
 
     @Override
@@ -41,7 +81,7 @@ public class StepMainActivity extends AppCompatActivity implements SensorEventLi
     @Override
     public void step(long timeNs) {
         numSteps++;
-
+        //TvSteps.setText(TEXT_NUM_STEPS + numSteps);
     }
 
 }
