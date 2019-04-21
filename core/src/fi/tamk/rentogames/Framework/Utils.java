45c52b9e4da50fc897186c4864841fb5f754b47e
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2019-04-21 22:44:56 +0300

    Utils javadoc

--- core/src/fi/tamk/rentogames/Framework/Utils.java
+++ core/src/fi/tamk/rentogames/Framework/Utils.java
@@ -3,25 +3,19 @@ package fi.tamk.rentogames.Framework;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 
-/*
-This class holds general utilities
- */
 /**
+ * Class for useful methods accessible from all classes.
+ *
  * @author Lauri Latva-Kyyny
  * @author  Miko Kauhanen
  * @version 1.0
  */
 public class Utils {
-    // Method for creating a random number
-    private float randomNumber(float min, float max) {
-        return MathUtils.random(min, max);
-    }
-
     /**
-     * @param tr
-     * @param cols
-     * @param rows
-     * @return
+     * @param tr texture region containing animation texture sheet in 2D-array
+     * @param cols amount of columns in animation texture sheet
+     * @param rows amount of rows in animation texture sheet
+     * @return animation textures split to 1D-array
      */
     // Method for making 2D array to 1D array
     public static TextureRegion[] toTextureArray(TextureRegion[][] tr, int cols, int rows)  {
